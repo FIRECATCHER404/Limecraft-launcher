a4bc91a89bab9e45341612d9824bf5e56e029de4
Firecatcher_303 <therockhound5833@outlook.com>
2026-09-20 16:07:59 -0700

    Delete src/main directory
    deleted files

--- src/main/java/com/limecraft/launcher/auth/DeviceCodeInfo.java
+++
@@ -1,9 +0,0 @@
-package com.limecraft.launcher.auth;
-
-public record DeviceCodeInfo(
-        String userCode,
-        String verificationUri,
-        String message,
-        String deviceCode,
-        int intervalSeconds
-) {}
